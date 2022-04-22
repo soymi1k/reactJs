16af7ae6250b20f95fbfb847a6e36d68c762bf1b
soymi1k <soymilkneigh@gmail.com>
2022-04-22 15:54:38 +0900

    module.css, useState

--- reactjsbeginner/src/App.js
+++ reactjsbeginner/src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import styles from "./App.module.css";
+import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log("call an api");
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome back!</h1>
+        <h1 className={styles.title}>Welcome back!</h1>
+        <h2>{counter}</h2>
+        <button onClick={onClick}>click me</button>
+        <Button text={"Hey"} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
